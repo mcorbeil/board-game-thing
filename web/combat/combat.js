8a14bb404862e1fc6187d5b24daf748c78d7af8b
mcorbeil <michael.corbeil@gmail.com>
2013-12-08 03:51:19 -0500

    Victory logic, loss assessment, auto-losses
    Added lots of shit here, should be working for basic battles
    
    will add more to this tomorrow and refactor further

--- web/combat/combat.js
+++ web/combat/combat.js
@@ -2,13 +2,14 @@
 Army class
 This should have all units defined eventually, currently keeping the base small
  */
+var VerboseConsole = true;
 function Army( Units ){
 	this.units =
 	{
 		"Armor"	:
 		{
 			"type"		:	"Armor"		,
-			"attack" 	:	3			,
+			"attack"	:	3			,
 			"defend"	:	3			,
 			"number"	: 	0
 		},
@@ -21,36 +22,39 @@ function Army( Units ){
 		},
 		"MansPaired"	:
 		{
-			"type"		:	"Mans"		,
+			"type"		:	"MansPaired",
 			"attack"	:	2			,
 			"defend"	:	2			,
 			"number"	:	0
 		},
 		"Trucks"	:
 		{
-			"type"		:	"Mans"		,
+			"type"		:	"Trucks"		,
 			"attack"	:	1			,
 			"defend"	:	2			,
 			"number"	:	0
 		},
 		"TrucksPaired"	:
 		{
-			"type"		:	"Mans"		,
+			"type"		:	"TrucksPaired",
 			"attack"	:	2			,
 			"defend"	:	2			,
 			"number"	:	0
 		},
 		"Artillery"	:
-			{
-				"type"		:	"Artillery"	,
-				"attack"	:	2			,
-				"defend"	:	2			,
-				"number"	:	0
-			}
-	};
+		{
+			"type"		:	"Artillery"	,
+			"attack"	:	2			,
+			"defend"	:	2			,
+			"number"	:	0
+		}
+	}
+	;
+	this.totalUnits = 0;
 
 	for( var i = 0 ; i < Units.length; i++ ) {
-		this.units[ Units[i]["type"] ].number = [Units[i]["number"]];
+		this.units[ Units[i]["type"] ].number = Units[i]["number"];
+		this.totalUnits += Units[i]["number"];
 	}
 
 	// After we have set up a list of units in the army, lets search for artillery pairings
@@ -66,6 +70,7 @@ function Army( Units ){
 		}
 		else {
 			// If we have no more Mans or Trucks to pair, break out of this loop!
+			alert("Breaking out of artilleryPairLoop");
 			break;
 		}
 	}
@@ -77,7 +82,14 @@ var attackingArmy =
 		[
 			{
 				"type"	: "Armor",
-				"number": 19
+				"number": 9
+			},
+			{	"type"	: "Artillery",
+				"number":2
+			},
+			{
+				"type"	: "Mans",
+				"number":3
 			}
 		]);
 
@@ -86,7 +98,7 @@ var defendingArmy =
 		[
 			{
 				"type"	:	"Mans",
-				"number":	22
+				"number":	3
 			}
 		]
 	);
@@ -98,47 +110,159 @@ $( document ).ready( function() {
 });
 
 /*
- * This function runs a round of combat
+ * This function runs rounds of combat until one or both sides perish!
 */
 function runCombat( attackingArmy, defendingArmy ) {
-	var myDie = 6;
-	var attackHits = 0;
-	var defendHits = 0;
-	$.each( attackingArmy.units, function(index, value) {
-		for( var i = 0; i < attackingArmy.units[index].number; i++ ) {
-			myDie = getRandomInt( 1 , 6 );
-			if( myDie <= attackingArmy.units[index].attack ) {
-				attackHits ++;
-				logCombat( "Attacking" , attackingArmy.units[index].type , myDie , "Hit!" )
+	var battleOver = false;
+	// Wrapped in a while to auto-combat to the death!
+	while( battleOver === false ) {
+		var myDie = 6;
+		var attackHits = 0;
+		var defendHits = 0;
+		$.each( attackingArmy.units, function( index, unit) {
+			for( var i = 0; i < unit.number; i++ ) {
+				myDie = getRandomInt( 1 , 6 );
+				if( myDie <= unit.attack ) {
+					attackHits ++;
+					logCombat( "Attacking" , unit.type , myDie , "Hit!" )
+				}
+				else {
+					logCombat( "Attacking" , unit.type , myDie , "Missed!" )
+				}
 			}
-			else {
-				logCombat( "Attacking" , attackingArmy.units[index].type , myDie , "Missed!" )
+		});
+		alert( "Attacker has scored " + attackHits + " hits this round" );
+
+		$.each( defendingArmy.units, function( index, unit ) {
+			for( var i = 0; i < unit.number; i++ ) {
+				myDie = getRandomInt( 1 , 6 );
+				if( myDie <= unit.defend ) {
+					defendHits++;
+					logCombat( "Defending" , unit.type , myDie , "Hit!" )
+				}
+				else {
+					logCombat( "Defending" , unit.type , myDie , "Missed!" )
+				}
 			}
+		});
+		alert( "Defender has scored " + defendHits + " hits this round" );
+		assessHits({ "attackHits" : attackHits , "defendHits" : defendHits});
+		battleOver = assessVictory();
+		if( battleOver !== false ) {
+			alert( battleOver + " has slaughtered there enemies" );
 		}
-	});
-	alert( "Attacker has scored " + attackHits + " hits this round" );
-	
-	$.each( defendingArmy.units, function( index, value ) {
-		for( var i = 0; i < defendingArmy.units[index].number; i++ ) {
-			myDie = getRandomInt( 1 , 6 );
-			if( myDie <= defendingArmy.units[index].defend ) {
-				defendHits++;
-				logCombat( "Defending" , defendingArmy.units[index].type , myDie , "Hit!" )
-			}
-			else {
-				logCombat( "Defending" , defendingArmy.units[index].type , myDie , "Missed!" )
-			}
-		}
-	});
-	alert( "Defender has scored " + defendHits + " hits this round" );
-	alert( "See console.log for detailed results");
-	return { "attackHits" : attackHits , "defendHits" : defendHits};
+	}
 }
 
+/*
+ * Simple function to dump combat results to console.log
+ * Set VerboseConsole ^^ at top of file to "false" to silence this
+ */
 function logCombat(attack_defend, type, roll, result) {
-	console.log( attack_defend + " " + type + " rolled a " + roll + " and " + result );
+	if( VerboseConsole ) {
+		console.log( attack_defend + " " + type + " rolled a " + roll + " and " + result );
+	}
 }
 
+/*
+ * The original idea here is to auto-remove units, in certain cases, unit removal is vital to combat playing out, we
+ * should consider making this interactive at a later point
+ */
+function assessHits( hitData ) {
+	// Again, loops may not be efficient, but they sure are clear to follow and clean to write
+	for( var i = 0; i < hitData.defendHits; i++ ) {
+		if( attackingArmy.units.Mans.number > 0 ) {
+			logDeath( "Mans" , "Attacker", i + 1 );
+			attackingArmy.units.Mans.number --;
+		}
+		else if( attackingArmy.units.MansPaired.number > 0 ) {
+			attackingArmy.units.MansPaired.number --;
+			logDeath( "MansPaired" , "Attacker" , i + 1 );
+		}
+		else if( attackingArmy.units.Trucks.number > 0 ) {
+			attackingArmy.units.Trucks.number --;
+			logDeath( "Trucks" , "Attacker" , i + 1 );
+		}
+		else if( attackingArmy.units.TrucksPaired.number > 0 ) {
+			logDeath( "TrucksPaired" , "Attacker" , i + 1 );
+			attackingArmy.units.TrucksPaired.number --;
+		}
+		else if( attackingArmy.units.Armor.number > 0 ) {
+			attackingArmy.units.Armor.number --;
+			logDeath( "Armor" , "Attacker" , i + 1 );
+		}
+		else {
+			// No more units to lose, break out of loop, army is dead
+			logDeath( "Attacker Has No Units","Attacker" , i + 1 );
+			break;
+		}
+		attackingArmy.totalUnits --;
+	}
+	for( var j = 0; j < hitData.attackHits; j++ ) {
+		if( defendingArmy.units.Mans.number > 0 ) {
+			defendingArmy.units.Mans.number --;
+			logDeath( "Mans" , "Defender" , j + 1 );
+		}
+		else if( defendingArmy.units.MansPaired.number > 0 ) {
+			defendingArmy.units.MansPaired.number --;
+			logDeath( "MansPaired" , "Defender" , j + 1 );
+		}
+		else if( defendingArmy.units.Trucks.number > 0 ) {
+			defendingArmy.units.Trucks.number --;
+			logDeath( "Trucks" , "Defender" , j + 1 );
+		}
+		else if( defendingArmy.units.TrucksPaired.number > 0 ) {
+			defendingArmy.units.TrucksPaired.number --;
+			logDeath( "TrucksPaired" , "Defender" , j + 1 );
+		}
+		else if( defendingArmy.units.Armor.number > 0 ) {
+			defendingArmy.units.Armor.number --;
+			logDeath( "Armor" , "Defender" , j + 1 );
+		}
+		else {
+			// No more units to lose, break out of loop, army is dead
+			logDeath( "Defender Has No Units" , "Defender" , j + 1 );
+			break;
+		}
+		defendingArmy.totalUnits --;
+	}
+}
+function logDeath( type, attack_defend, number ) {
+	if( VerboseConsole ) {
+		console.log( attack_defend + " is losing " + number + " " + type );
+	}
+}
+
+/*
+ * It seems like we need to check to verify the battle is over, if both sides have units battle will continue!
+ */
+function assessVictory() {
+	var attackerPresent = false;
+	var defenderPresent = false;
+
+	if( attackingArmy.totalUnits > 0 ) {
+		attackerPresent = true;
+	}
+
+	if( defendingArmy.totalUnits > 0 ) {
+		defenderPresent = true;
+	}
+
+	if( attackerPresent && defenderPresent ) {
+		return false;
+	}
+	else if( attackerPresent ) {
+		return "Attacker";
+	}
+	else if( defenderPresent ) {
+		return "Defender"
+	}
+	else {
+		return "Error";
+	}
+}
+
+// If anyone would like to propose different RNG logic, feel free
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // Returns a random integer between min and max
