e15790c6f726810700ab5c2dbe284186a5a7f531
mcorbeil <michael.corbeil@gmail.com>
2013-12-08 02:27:26 -0500

    Many improvements to namings
    Improved the way data gets input to Army creation
    
    Added artillery pairing logic

--- web/combat/combat.js
+++ web/combat/combat.js
@@ -1,70 +1,142 @@
-function territory (){
+/*
+Army class
+This should have all units defined eventually, currently keeping the base small
+ */
+function Army( Units ){
 	this.units =
-		[
+	{
+		"Armor"	:
+		{
+			"type"		:	"Armor"		,
+			"attack" 	:	3			,
+			"defend"	:	3			,
+			"number"	: 	0
+		},
+		"Mans"	:
+		{
+			"type"		:	"Mans"		,
+			"attack"	:	1			,
+			"defend"	:	2			,
+			"number"	:	0
+		},
+		"MansPaired"	:
+		{
+			"type"		:	"Mans"		,
+			"attack"	:	2			,
+			"defend"	:	2			,
+			"number"	:	0
+		},
+		"Trucks"	:
+		{
+			"type"		:	"Mans"		,
+			"attack"	:	1			,
+			"defend"	:	2			,
+			"number"	:	0
+		},
+		"TrucksPaired"	:
+		{
+			"type"		:	"Mans"		,
+			"attack"	:	2			,
+			"defend"	:	2			,
+			"number"	:	0
+		},
+		"Artillery"	:
 			{
-				"type":"Armor",
-				"attack":3,
-				"defend":3,
-				"number":3
-			},
-			{
-				"type":"Infantry",
-				"attack":1,
-				"defend":2,
-				"number":6
+				"type"		:	"Artillery"	,
+				"attack"	:	2			,
+				"defend"	:	2			,
+				"number"	:	0
 			}
-		];
+	};
+
+	for( var i = 0 ; i < Units.length; i++ ) {
+		this.units[ Units[i]["type"] ].number = [Units[i]["number"]];
+	}
+
+	// After we have set up a list of units in the army, lets search for artillery pairings
+	// I am aware this may not be as efficient as an if-else block, but this is the cleanest to write
+	for( var j = 0; j < this.units.Artillery.number; j++) {
+		if( this.units.Trucks.number > 0 ) {
+			this.units.Trucks.number --;
+			this.units.TrucksPaired.number ++;
+		}
+		else if( this.units.Mans.number > 0 ) {
+			this.units.Mans.number --;
+			this.units.MansPaired.number ++;
+		}
+		else {
+			// If we have no more Mans or Trucks to pair, break out of this loop!
+			break;
+		}
+	}
 }
 
 
-var attackingTerritory = new territory();
+var attackingArmy =
+	new Army(
+		[
+			{
+				"type"	: "Armor",
+				"number": 19
+			}
+		]);
 
-var defendingTerritory = new territory();
+var defendingArmy =
+	new Army(
+		[
+			{
+				"type"	:	"Mans",
+				"number":	22
+			}
+		]
+	);
 
 $( document ).ready( function() {
 	$("#startCombat").click( function() {
-		runCombat( attackingTerritory, defendingTerritory );
+		runCombat( attackingArmy, defendingArmy );
 	});
 });
 
 /*
  * This function runs a round of combat
 */
-function runCombat( attackingTerritory, defendingTerritory ) {
+function runCombat( attackingArmy, defendingArmy ) {
 	var myDie = 6;
 	var attackHits = 0;
 	var defendHits = 0;
-	var Log = {};
-	$.each( attackingTerritory.units, function(index, value) {
-		for( var i = 0; i < attackingTerritory.units[index].number; i++ ) {
+	$.each( attackingArmy.units, function(index, value) {
+		for( var i = 0; i < attackingArmy.units[index].number; i++ ) {
 			myDie = getRandomInt( 1 , 6 );
-			if( myDie <= attackingTerritory.units[index].attack ) {
+			if( myDie <= attackingArmy.units[index].attack ) {
 				attackHits ++;
-				Log["attacking-" + index + "-" + i] = { "roll" : myDie , "unit" : attackingTerritory.units[index].type , "result" : "Hit!" };
+				logCombat( "Attacking" , attackingArmy.units[index].type , myDie , "Hit!" )
 			}
 			else {
-				Log["attacking-" + index + "-" + i] = { "roll" : myDie , "unit" : attackingTerritory.units[index].type , "result" : "Miss!" };
+				logCombat( "Attacking" , attackingArmy.units[index].type , myDie , "Missed!" )
 			}
 		}
 	});
 	alert( "Attacker has scored " + attackHits + " hits this round" );
 	
-	$.each( defendingTerritory.units, function( index, value ) {
-		for( var i = 0; i < defendingTerritory.units[index].number; i++ ) {
+	$.each( defendingArmy.units, function( index, value ) {
+		for( var i = 0; i < defendingArmy.units[index].number; i++ ) {
 			myDie = getRandomInt( 1 , 6 );
-			if( myDie <= defendingTerritory.units[index].defend ) {
+			if( myDie <= defendingArmy.units[index].defend ) {
 				defendHits++;
-				Log["defending-" + index + "-" + i] = {"roll" : myDie , "unit" : defendingTerritory.units[index].type , "result" : "Hit!"};
+				logCombat( "Defending" , defendingArmy.units[index].type , myDie , "Hit!" )
 			}
 			else {
-				Log["defending-" + index + "-" + i] = {"roll" : myDie , "unit" : defendingTerritory.units[index].type , "result" : "Miss!"};
+				logCombat( "Defending" , defendingArmy.units[index].type , myDie , "Missed!" )
 			}
 		}
 	});
 	alert( "Defender has scored " + defendHits + " hits this round" );
-	console.log( Log );
 	alert( "See console.log for detailed results");
 	return { "attackHits" : attackHits , "defendHits" : defendHits};
+}
+
+function logCombat(attack_defend, type, roll, result) {
+	console.log( attack_defend + " " + type + " rolled a " + roll + " and " + result );
 }
 
 
